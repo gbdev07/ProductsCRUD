dfda20bbabe253735182575f63f55ec88769355e
Gabriel <gbdev07@gmail.com>
2022-11-23 18:03:00 -0300

    Products CRUD

---
+++ ProductsCRUD/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsCRUD.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=CrudProducts;Integrated Security=True");
+        }
+    }
+}
---
+++ ProductsCRUD/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsCRUD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+    }
+}
